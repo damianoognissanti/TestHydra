18e7f25534f1e3aa227b44add536cc8d41b402f7
Damiano Ognissanti <ognissanti@hotmail.se>
2024-02-02 10:41:10 +0100

    test

--- flake.nix
+++ flake.nix
@@ -2,11 +2,12 @@
   description = "Test Hydra";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-    hardware.url = "github:NixOS/nixos-hardware/master";
+    #hardware.url = "github:NixOS/nixos-hardware/master";
   };
   outputs = { self, nixpkgs, hardware, ... }: {
     packages.x86_64-linux = {
-      microsoft-surface-pro-intel = hardware.nixosModules.microsoft-surface-pro-intel;
+      #microsoft-surface-pro-intel = hardware.nixosModules.microsoft-surface-pro-intel;
+      hello = nixpkgs.hello;
 
     };
 
